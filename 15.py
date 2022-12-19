ca9c48ad9ef7072a97860e326897cced4aed770f
Bősz Norbert <norbert.bosz@nitrowise.com>
2022-12-19 04:51:23 +0100

    Day 15

--- 15.py
+++ 15.py
@@ -11,25 +11,15 @@ class Sensor:
         self.by = by
         self.steps = abs(x - bx) + abs(y - by)
 
+    def can_reach(self, x, y):
+        return self.steps >= abs(self.x - x) + abs(self.y - y)
+
     def covered(self, row):
-        cov = set()
+        covered = set()
         distance = abs(row - self.y)
         for i in range(-(self.steps - distance), self.steps - distance + 1):
-            cov.add(self.x + i)
-        return cov
-
-    def __repr__(self):
-        return '[' + str(self.x) + ':' + str(self.y) + ', s: ' + str(self.steps) + ']'
-
-
-sensors = []
-beacons = set()
-for line in lines:
-    s, b = line.split(': closest beacon is at x=')
-    s_x, s_y = map(int, s.strip('Sensor at x=').split(', y='))
-    b_x, b_y = map(int, b.split(', y='))
-    sensors.append(Sensor(s_x, s_y, b_x, b_y))
-    beacons.add((b_x, b_y))
+            covered.add(self.x + i)
+        return covered
 
 
 def covered_in_row(checked_row):
@@ -42,4 +32,44 @@ def covered_in_row(checked_row):
     return len(covered) - beacons_in_row
 
 
+def find_hidden_beacon():
+    for sensor in sorted(sensors, key=lambda s: s.steps):
+        edge = sensor.steps + 1
+        for i in range(-edge, edge + 1):
+            j = edge - abs(i)
+            x, y = sensor.x + i, sensor.y + j
+            if not can_any_sensors_reach(x, y) and not is_outer(x, y):
+                return x, y
+            j = -edge + abs(i)
+            x, y = sensor.x + i, sensor.y + j
+            if not can_any_sensors_reach(x, y) and not is_outer(x, y):
+                return x, y
+
+
+def is_outer(x, y):
+    for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        if not can_any_sensors_reach(x + i, y + j):
+            return True
+    return False
+
+
+def can_any_sensors_reach(x, y):
+    for sensor in sensors:
+        if sensor.can_reach(x, y):
+            return True
+    return False
+
+
+sensors = []
+beacons = set()
+for line in lines:
+    s, b = line.split(': closest beacon is at x=')
+    s_x, s_y = map(int, s.strip('Sensor at x=').split(', y='))
+    b_x, b_y = map(int, b.split(', y='))
+    sensors.append(Sensor(s_x, s_y, b_x, b_y))
+    beacons.add((b_x, b_y))
+
 print(covered_in_row(2000000))
+
+hidden_beacon = find_hidden_beacon()
+print(4000000 * hidden_beacon[0] + hidden_beacon[1])
